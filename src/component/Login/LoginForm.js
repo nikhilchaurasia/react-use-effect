5fead69ab652e9cb5a44af6b52314e351fb35dfb
Nikhil Chaurasiya <nikhilchaurasiya@administrators-MacBook-Pro.local>
2023-06-10 13:12:20 +0530

    Added timer function in use effect

--- src/component/Login/LoginForm.js
+++ src/component/Login/LoginForm.js
@@ -1,57 +1,85 @@
-import styles from './LoginFrom.module.css'
-import { useEffect, useState } from 'react';
-import Card from '../../UI/Card'
-import Button from '../../UI/Button';
+import styles from "./LoginFrom.module.css";
+import { useEffect, useState } from "react";
+import Card from "../../UI/Card";
+import Button from "../../UI/Button";
 
-const LoginForm = props => {
+const LoginForm = (props) => {
+  const [username, setUsername] = useState("");
+  const [isvalidUsername, setIsValidUsername] = useState(false);
+  const [password, setPassword] = useState("");
+  const [isValidPassword, setValidPassword] = useState(false);
+  const [isValidForm, setValidForm] = useState(false);
 
-    const [username, setUsername] = useState('');
-    const [isvalidUsername, setIsValidUsername] = useState(false);
-    const [password, setPassword] = useState('');
-    const [isValidPassword, setValidPassword] = useState(false);
-    const [isValidForm, setValidForm] = useState(false);
-
-    useEffect(() => {
-        setValidForm(username.trim().length > 3 && password.trim().length > 7)
-    }, [username, password])
-
-    const usernameChangeHandler = (event) => {
-        setUsername(event.target.value)
+  useEffect(() => {
+    const timeoutIdentifier = setTimeout(() => {
+      console.log("Use effect called");
+      setValidForm(username.trim().length > 3 && password.trim().length > 7);
+    }, 500);
+    return () => {
+        console.log("Cleanup called");
+        clearTimeout(timeoutIdentifier)
     }
+  }, [username, password]);
 
-    const passwordChangeHandler = (event) => {
-        setPassword(event.target.value)
-    }
+  const usernameChangeHandler = (event) => {
+    setUsername(event.target.value);
+  };
 
-    const loginhandler = () => {
-        props.onLogin(username, password);
-    };
+  const passwordChangeHandler = (event) => {
+    setPassword(event.target.value);
+  };
 
-    const validUsernameHandler = () => {
-        setIsValidUsername(username.trim().length > 3)
-    }
+  const loginhandler = () => {
+    props.onLogin(username, password);
+  };
 
-    const validPasswordHandler = () => {
-        setValidPassword(password.trim().length > 7)
-    }
+  const validUsernameHandler = () => {
+    setIsValidUsername(username.trim().length > 3);
+  };
 
-    return (
-        <Card className={styles.login}>
-            <form onSubmit={loginhandler}>
-                <div className={`${styles.control} ${isvalidUsername === false ? styles.invalid : ''}`}>
-                    <label htmlFor="username">Username</label>
-                    <input value={username} type="text" id="username" onChange={usernameChangeHandler} onBlur={validUsernameHandler}/>
-                </div>
-                <div className={`${styles.control} ${isValidPassword === false ? styles.invalid : ''}`}>
-                    <label htmlFor="password">Password</label>
-                    <input type="password" value={password} id="password" onChange={passwordChangeHandler} onBlur={validPasswordHandler}/>
-                </div>
-                <div className={styles['control']}>
-                    <Button type="submit" disabled={!isValidForm} >Login</Button>
-                </div>
-            </form>
-        </Card>
-    )
-}
+  const validPasswordHandler = () => {
+    setValidPassword(password.trim().length > 7);
+  };
+
+  return (
+    <Card className={styles.login}>
+      <form onSubmit={loginhandler}>
+        <div
+          className={`${styles.control} ${
+            isvalidUsername === false ? styles.invalid : ""
+          }`}
+        >
+          <label htmlFor="username">Username</label>
+          <input
+            value={username}
+            type="text"
+            id="username"
+            onChange={usernameChangeHandler}
+            onBlur={validUsernameHandler}
+          />
+        </div>
+        <div
+          className={`${styles.control} ${
+            isValidPassword === false ? styles.invalid : ""
+          }`}
+        >
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            value={password}
+            id="password"
+            onChange={passwordChangeHandler}
+            onBlur={validPasswordHandler}
+          />
+        </div>
+        <div className={styles["control"]}>
+          <Button type="submit" disabled={!isValidForm}>
+            Login
+          </Button>
+        </div>
+      </form>
+    </Card>
+  );
+};
 
 export default LoginForm;
